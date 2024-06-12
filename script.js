b15eb6bf5d85703c8449eab1e0cf1988c96a994a
squishysquid30 <evelyn.a.g.biggs@gmail.com>
2024-06-12 10:15:26 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Home</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <!--
+      Need a visual blank slate?
+      Remove all code in `styles.css`!
+    -->
+    <link rel="stylesheet" href="styles.css" />
+    <script type="module" src="script.js"></script>
+  </head>
+  <body>
+<!--Heading-->
+    <h1>About Me</h1>
+<!--image-->
+<!--paragraph-->
+    <p></p>
+<!--list-->
+    <ol>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+</body>
+</html>
